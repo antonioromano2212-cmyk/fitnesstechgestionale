477a941e306f7ae2e59b52637bb309724e0f900e
Antonio Romano <anspibn@gmail.com>
2026-05-18 23:43:22 +0200

    Fix calendario definitivo

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -1,9 +1,7 @@
 export default function CalendarioPage() {
   return (
-    <main className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-4xl font-bold text-black">
-        Calendario
-      </h1>
-    </main>
+    <div>
+      Calendario
+    </div>
   );
 }
